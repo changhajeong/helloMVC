f376dd3c225a1a3b73a9074deee8c54b647f2552
changhajeong <71754210+changhajeong@users.noreply.github.com>
2020-09-26 20:16:02 +0900

    Initial commit

---
+++ WebContent/view/registerSuccess.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>You registered successfully</title>
+</head>
+<body>
+<h1> You registered successfully</h1>
+	<ol>
+		<li> Id: ${id}</li>
+		<li> password: ${password}</li>
+		<li> gender: ${gender}</li>
+		<li> Name: ${name}</li>
+		<li> Email: ${email}</li>
+	</ol>
+	<p>
+		<a href="/hw1-helloMVC/index.jsp"> go to home page </a>
+	</p>
+
+
+</body>
+</html>
---
+++ src/controller/DoRegister.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+
+	
+		
+		CustomerService service = (CustomerService) CustomerService.getInstance();
+		service.addCustomer(new Customer(id,password,name,gender,email));
+		request.setAttribute("id", id);
+		request.setAttribute("password", password);
+		request.setAttribute("name", name);
+		request.setAttribute("gender", gender);
+		request.setAttribute("email", email);
+	
+			
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/registerSuccess.jsp");
+		dispatcher.forward(request, response);
+	}
+}
+
